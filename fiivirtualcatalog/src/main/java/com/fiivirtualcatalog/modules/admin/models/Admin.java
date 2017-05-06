5ceac5942153ef5aa50d84d90c0fbe4eba11f144
Ovidiu Benone CHILE <ovidiu.chile@info.uaic.ro>
2017-05-06 21:33:12 +0300

    Added getAllByName method to admin

--- fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/models/Admin.java
+++ fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/models/Admin.java
@@ -6,21 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Admin extends User{
     @Autowired
-    UserService userService;
-    List<User> getAllAdmins(){
-        List<User> users = new ArrayList<>();
-        for (User user : this.userService.getAll())
-            if (user.getRole().toLowerCase() == "admin")
-                users.add(user);
-        return users;
-    }
+    private UserService userService;
 
     Admin(User user){
         this.setId(user.getId());
         this.setName(user.getName());
         this.setRole("admin");
     }
+
+    List<User> getAllAdmins(){
+        List<User> users = new ArrayList<>();
+        for (User user : this.userService.getAll())
+            if (user.getRole().toLowerCase().equals("admin"))
+                users.add(user);
+        return users;
+    }
+    List<User> getAllByName(String name){
+        Pattern pattern = Pattern.compile("*" + name + "*");
+        Matcher matcher;
+        List<User> users = new ArrayList<>();
+        for (User user : this.userService.getAll()){
+            matcher = pattern.matcher(user.getName());
+            if (matcher.matches())
+                users.add(user);
+        }
+        return users;
+    }
+    
 }
