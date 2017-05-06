2402aa924aa2143b1721ad4d403f509917c8cb10
Ovidiu Benone CHILE <ovidiu.chile@info.uaic.ro>
2017-05-06 21:28:06 +0300

    Added getAllAdmins method and constructor by user for admin

--- fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/models/Admin.java
+++ fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/models/Admin.java
@@ -1,4 +1,26 @@
 package com.fiivirtualcatalog.modules.admin.models;
 
-public class Admin {
+import com.fiivirtualcatalog.modules.user.models.User;
+import com.fiivirtualcatalog.modules.user.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Admin extends User{
+    @Autowired
+    UserService userService;
+    List<User> getAllAdmins(){
+        List<User> users = new ArrayList<>();
+        for (User user : this.userService.getAll())
+            if (user.getRole().toLowerCase() == "admin")
+                users.add(user);
+        return users;
+    }
+
+    Admin(User user){
+        this.setId(user.getId());
+        this.setName(user.getName());
+        this.setRole("admin");
+    }
 }
