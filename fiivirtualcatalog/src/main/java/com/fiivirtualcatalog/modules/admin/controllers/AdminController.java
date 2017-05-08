d6ced9ee9d4ef3679227bf03c8d26c48cf8a71f4
Ovidiu Benone CHILE <ovidiu.chile@info.uaic.ro>
2017-05-08 18:20:11 +0300

    added deleted and update to admin controller

--- fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/controllers/AdminController.java
+++ fiivirtualcatalog/src/main/java/com/fiivirtualcatalog/modules/admin/controllers/AdminController.java
@@ -1,7 +1,6 @@
 package com.fiivirtualcatalog.modules.admin.controllers;
 
 import com.fiivirtualcatalog.modules.DTOs.UserDTO;
-import com.fiivirtualcatalog.modules.admin.services.AdminService;
 import com.fiivirtualcatalog.modules.transformers.Transformer;
 import com.fiivirtualcatalog.modules.transformers.UserTransformer;
 import com.fiivirtualcatalog.modules.user.models.User;
@@ -9,7 +8,6 @@ import com.fiivirtualcatalog.modules.user.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,13 +18,13 @@ import java.util.List;
 
 public class AdminController {
     @Autowired
-    UserService userService;
+    UserService service;
 
     private Transformer<User, UserDTO> transformer = new UserTransformer();
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> get() {
-        List<User> users = this.userService.getAll();
+        List<User> users = this.service.getAll();
         if (users.isEmpty()) {
             return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
         }
@@ -42,11 +40,11 @@ public class AdminController {
             return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<User>>(admins, HttpStatus.OK);
-}
+    }
 
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     public ResponseEntity<User> getUser(@PathVariable("userId") Long userId) {
-        User user = this.userService.getById(userId);
+        User user = this.service.getById(userId);
         if (user == null) {
             return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
         }
@@ -60,7 +58,34 @@ public class AdminController {
     public ResponseEntity<User> createAdmin(@RequestBody UserDTO user) {
         User tempUser = transformer.toModel(user);
         tempUser.setRole("admin");
-        User savedUser = this.userService.save(tempUser);
+        User savedUser = this.service.save(tempUser);
         return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value="/{userId}", method=RequestMethod.PUT)
+    public ResponseEntity<User> updateById(@PathVariable("userId") Long updateId, @RequestBody UserDTO update) {
+        User user = this.service.getById(updateId);
+        if (user == null || !user.getRole().equalsIgnoreCase("admin")) {
+            return new ResponseEntity<User>(HttpStatus.NO_CONTENT);
+        }
+        User userUpdate=transformer.toModel(update);
+
+        user.setRole(userUpdate.getRole());
+        user.setName(userUpdate.getName());
+        User userSaved = this.service.save(user);
+        return new ResponseEntity<User>(userSaved, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{userId}",method = RequestMethod.DELETE)
+    public ResponseEntity<List<User>> deleteById(@PathVariable("userId") Long deleteId){
+
+        User user = this.service.getById(deleteId);
+        if (user == null || !user.getRole().equalsIgnoreCase("admin")) {
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+        }
+
+        this.service.delete(deleteId);
+        List<User> del = this.service.getAll();
+        return new ResponseEntity<List<User>>(del, HttpStatus.OK);
     }
 }
