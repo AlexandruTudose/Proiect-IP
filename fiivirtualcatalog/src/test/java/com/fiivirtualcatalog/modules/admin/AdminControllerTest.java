c4ed59505e58009bebe812af55020d76c7c5fbef
Alexandru Dochitoiu <alexandru.dochitoiu@info.uaic.ro>
2017-05-09 23:37:46 +0300

    first unit test using mockito

---
+++ fiivirtualcatalog/src/test/java/com/fiivirtualcatalog/modules/admin/AdminControllerTest.java
@@ -0,0 +1,32 @@
+package com.fiivirtualcatalog.modules.admin;
+
+import com.fiivirtualcatalog.modules.admin.controllers.AdminController;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+public class AdminControllerTest {
+    @InjectMocks
+    private AdminController controller;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+        mockMvc.perform(get("//v1/admins/15")).andDo(print());
+    }
+}
