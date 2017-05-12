f3bbb746abdaf610c8d9cdbcef065aff947e5bc0
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-05-12 18:55:05 +0300

    Test for tipul orei(enum)

---
+++ modululCheckIn/fiivirtualcatalog/src/test/java/com/fiivirtualcatalog/modules/checkin/enums/ClassTypeTest.java
@@ -0,0 +1,36 @@
+package com.fiivirtualcatalog.modules.checkin.enums;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClassTypeTest {
+
+    @Test
+    public void customTestForVerifyingThings(){
+        ClassType.valueOf(ClassType.Seminar.toString());
+        ClassType.valueOf(ClassType.Laboratory.toString());
+        ClassType.valueOf(ClassType.Course.toString());
+    }
+
+    @Test
+    public void forAnInputStringSeminarShouldReturnSeminar(){
+        assertEquals(ClassType.toEnum("Seminar"),ClassType.Seminar);
+    }
+
+    @Test
+    public void forAnInputStringLaboratoryShouldReturnLaboratory(){
+        assertEquals(ClassType.toEnum("Laboratory"),ClassType.Laboratory);
+    }
+
+    @Test
+    public void forAnInputStringCourseShouldReturnCourse(){
+        assertEquals(ClassType.toEnum("Course"),ClassType.Course);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void forAnInputStringAnythingShouldReturnIllegalError(){
+        assertEquals(ClassType.toEnum("Anything"),ClassType.Course);
+    }
+}
