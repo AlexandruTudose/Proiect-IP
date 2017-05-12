32d1d0058198a11d64f293e79e713c987ccaca1a
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-05-12 18:43:36 +0300

    Update user test

---
+++ modululCheckIn/fiivirtualcatalog/src/test/java/com/fiivirtualcatalog/modules/user/models/UserTest.java
@@ -0,0 +1,35 @@
+package com.fiivirtualcatalog.modules.user.models;
+
+import com.fiivirtualcatalog.modules.user.models.User;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user= new User();
+    }
+
+    @Test
+    public void forUserWithId32ShouldReturn32(){
+        user.setId(32);
+        assertEquals(user.getId(),32);
+    }
+
+
+    @Test
+    public void forUserWithNameMAriaShouldReturnMaria(){
+        user.setName("Maria");
+        assertEquals(user.getName(),"Maria");
+    }
+
+    @Test
+    public void forUserWithRoleStudentShouldReturnStudent(){
+        user.setRole("Student");
+        assertEquals(user.getRole(),"Student");
+    }
+
+}
