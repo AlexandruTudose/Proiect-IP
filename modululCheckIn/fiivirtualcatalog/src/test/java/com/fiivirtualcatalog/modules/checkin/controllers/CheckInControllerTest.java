4e34676612323d0898b49e41575423f9633dd653
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-05-17 04:06:06 +0300

    Updated CheckInController tests

--- modululCheckIn/fiivirtualcatalog/src/test/java/com/fiivirtualcatalog/modules/checkin/controllers/CheckInControllerTest.java
+++ modululCheckIn/fiivirtualcatalog/src/test/java/com/fiivirtualcatalog/modules/checkin/controllers/CheckInControllerTest.java
@@ -1,6 +1,7 @@
 package com.fiivirtualcatalog.modules.checkin.controllers;
 
 import com.fiivirtualcatalog.FiiVirtualCatalogApplication;
+import com.fiivirtualcatalog.modules.checkin.dtos.CheckInGetAllDTO;
 import com.fiivirtualcatalog.modules.checkin.dtos.CheckInPostDTO;
 import com.fiivirtualcatalog.modules.checkin.enums.ClassType;
 import com.fiivirtualcatalog.modules.checkin.models.CheckIn;
@@ -39,15 +40,37 @@ public class CheckInControllerTest {
 
     @Test
     public void forCallingGetMethodShouldReturnStatusOK() {
+
+        CheckIn checkIn = new CheckIn();
+        checkIn.setId(23);
+
+        java.sql.Date currentTimestamp = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        checkIn.setCreateDate(currentTimestamp);
+
+        User user = new User();
+        user.setName("Maria");
+        user.setRole("Profesor");
+        userRepository.save(user);
+        checkIn.setUser(user);
+        checkIn.setSubject("Subject");
+        checkIn.setClassType("Course");
+        checkIn.setNumberOfCheckedInUsers(33);
+        checkIn.setFinishingFlag(true);
+
+        List<User> list = new ArrayList<>();
+        checkIn.setCheckedInUsers(list);
+
+        checkInRepository.save(checkIn);
+
         ResponseEntity actualResponse = new ResponseEntity(HttpStatus.OK);
         ResponseEntity responseEntity = checkInController.get();
-
         assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
     }
 
     @Test
     public void forCallingGetMethodShouldReturnStatusNOContent() {
         ResponseEntity actualResponse = new ResponseEntity(HttpStatus.NO_CONTENT);
+        userRepository.deleteAll();
         checkInRepository.deleteAll();
 
         ResponseEntity responseEntity = checkInController.get();
@@ -56,9 +79,129 @@ public class CheckInControllerTest {
     }
 
     @Test
+    public void forCallingGetByIdMethodShoulReturnStatusNoContent(){
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.NO_CONTENT);
+        userRepository.deleteAll();
+
+        ResponseEntity responseEntity = checkInController.getById((long) 1);
+
+        assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
+    public void forCallingGetByIdMethodShoulReturnStatusOk(){
+        CheckIn checkIn = new CheckIn();
+        checkIn.setId(23);
+
+        java.sql.Date currentTimestamp = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        checkIn.setCreateDate(currentTimestamp);
+
+        User user = new User();
+        user.setName("Maria");
+        user.setRole("Profesor");
+        userRepository.save(user);
+        checkIn.setUser(user);
+        checkIn.setSubject("Subject");
+        checkIn.setClassType("Course");
+        checkIn.setNumberOfCheckedInUsers(33);
+        checkIn.setFinishingFlag(true);
+
+        List<User> list = new ArrayList<>();
+        checkIn.setCheckedInUsers(list);
+
+        CheckIn savedCheckIn = checkInRepository.save(checkIn);
+
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.OK);
+
+        ResponseEntity responseEntity = checkInController.getById(savedCheckIn.getId());
+
+        assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
+    public void forCallingDeleteUserMethodShoulReturnStatusNotFound(){
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.NOT_FOUND);
+        userRepository.deleteAll();
+
+        ResponseEntity responseEntity = checkInController.deleteUser((long) 1,(long)23);
+
+        assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
+    public void forCallingDeleteUserIdMethodShoulReturnStatusForbidden(){
+        CheckIn checkIn = new CheckIn();
+        checkIn.setId(23);
+
+        java.sql.Date currentTimestamp = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        checkIn.setCreateDate(currentTimestamp);
+
+        User user = new User();
+        user.setName("Maria");
+        user.setRole("Profesor");
+        userRepository.save(user);
+        checkIn.setUser(user);
+        checkIn.setSubject("Subject");
+        checkIn.setClassType("Course");
+        checkIn.setNumberOfCheckedInUsers(33);
+        checkIn.setFinishingFlag(true);
+
+        List<User> list = new ArrayList<>();
+        checkIn.setCheckedInUsers(list);
+
+        CheckIn savedCheckIn = checkInRepository.save(checkIn);
+
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.FORBIDDEN);
+
+        ResponseEntity responseEntity = checkInController.deleteUser(savedCheckIn.getId(),(long) 23);
+
+        assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
+    public void forCallingDeleteUserIdMethodShoulReturnStatusCreated(){
+        CheckIn checkIn = new CheckIn();
+        checkIn.setId(23);
+
+        java.sql.Date currentTimestamp = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+        checkIn.setCreateDate(currentTimestamp);
+
+        User user = new User();
+        user.setName("Maria");
+        user.setRole("Profesor");
+        userRepository.save(user);
+        checkIn.setUser(user);
+        checkIn.setSubject("Subject");
+        checkIn.setClassType("Course");
+        checkIn.setNumberOfCheckedInUsers(33);
+        checkIn.setFinishingFlag(false);
+
+        User user1 = new User();
+        user1.setName("Maria");
+        user1.setRole("Student");
+        User userSaved = userRepository.save(user1);
+        List<User> list = new ArrayList<>();
+        list.add(user1);
+        checkIn.setCheckedInUsers(list);
+
+        CheckIn savedCheckIn = checkInRepository.save(checkIn);
+
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.CREATED);
+
+        ResponseEntity responseEntity = checkInController.deleteUser(savedCheckIn.getId(),userSaved.getId());
+
+        assertEquals(responseEntity.getStatusCode(), actualResponse.getStatusCode());
+    }
+
+    @Test
     public void forCallingCreateCheckInMethodShouldReturnStatusCreated() {
+
+        User user = new User();
+        user.setName("Maria");
+        user.setRole("Student");
+        User userSaved = userRepository.save(user);
         CheckInPostDTO checkInPostDTO = new CheckInPostDTO();
-        checkInPostDTO.setUserId(1);
+        checkInPostDTO.setUserId(userSaved.getId());
         checkInPostDTO.setSubject("Subject");
         checkInPostDTO.setClassType(ClassType.Course);
 
@@ -286,6 +429,14 @@ public class CheckInControllerTest {
 
         ResponseEntity actualResponse = new ResponseEntity(HttpStatus.CREATED);
         ResponseEntity responseEntity = checkInController.endCheckIn(userSaved.getId(), checkInSaved.getId());
+
+        assertEquals(responseEntity, actualResponse);
+    }
+
+    @Test
+    public void forCallingDeleteAllShoulReturnStatusOK(){
+        ResponseEntity actualResponse = new ResponseEntity(HttpStatus.OK);
+        ResponseEntity responseEntity = checkInController.deleteAll();
 
         assertEquals(responseEntity, actualResponse);
     }
